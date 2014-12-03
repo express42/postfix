6af380885c533c344166cf301041dc84a35274d9
Igor Kurochkin <ikurochkin@express42.com>
2014-12-03 13:50:59 +0300

    Version bump to 0.1.1

--- metadata.rb
+++ metadata.rb
@@ -1,9 +1,9 @@
-name              "postfix"
-maintainer        "LLC Express 42"
-maintainer_email  "info@express42.com"
-license           "MIT"
-description       "Installs and configures postfix servers"
-version           "0.1.0"
+name              'postfix'
+maintainer        'LLC Express 42'
+maintainer_email  'info@express42.com'
+license           'MIT'
+description       'Installs and configures postfix and DKIM. Provides LWRPs for managing multiple instances'
+version           '0.1.1'
 
-supports          "debian"
-supports          "ubuntu"
+supports          'debian'
+supports          'ubuntu'
