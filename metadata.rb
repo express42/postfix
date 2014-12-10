fd4f4401ae668cccd4b6aa64a19407cc4b673235
Igor Kurochkin <ikurochkin@express42.com>
2014-12-10 19:05:52 +0300

    Update metadata

--- metadata.rb
+++ metadata.rb
@@ -1,11 +1,11 @@
 name              'postfix'
 maintainer        'LLC Express 42'
-maintainer_email  'info@express42.com'
+maintainer_email  'cookbooks@express42.com'
 license           'MIT'
-description       'Installs and configures postfix and DKIM. Provides LWRPs for managing multiple instances'
+description       'Installs and configures postfix and DKIM. Provides LWRPs for managing multiple instances.'
 version           '0.1.1'
 
-recipe            'postfix::default', 'Do nothing'
-recipe            'postfix::default_server', 'Installs and configures default postfix instance'
+recipe            'postfix::default', 'Do nothing.'
+recipe            'postfix::default_server', 'Installs and configures default postfix instance.'
 
 supports          'ubuntu'
