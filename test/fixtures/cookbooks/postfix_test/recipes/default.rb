9bf6e1e25c5a3780beed1747a04733deb46d73a0
Igor Kurochkin <ikurochkin@express42.com>
2015-08-29 16:59:41 +0300

    Fix postfix_test recipe

--- test/fixtures/cookbooks/postfix_test/recipes/default.rb
+++ test/fixtures/cookbooks/postfix_test/recipes/default.rb
@@ -5,10 +5,12 @@ postfix_dkim 'test'
 
 postfix 'test1' do
   master_options(smtpd_port: 125)
+  options(mydomain: 'test1')
   action :create
 end
 
 postfix 'test2' do
   master_options(smtpd_port: 225)
+  options(mydomain: 'test2')
   action :create
 end
